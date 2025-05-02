b17e2c45730263a1e888e587411dc3b83131dd4d
Lucho367 <lucianoszmukler0@gmail.com>
2025-05-01 23:52:19 -0300

    cree una plataforma que al pisarla se caiga

---
+++ Assets/Scripts/PlataformaTemporal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaTemporal : MonoBehaviour
+{
+    
+
+  
+    private Animator _animator;
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<PlayerActions>())
+        {
+            
+            _animator.SetBool("activado", false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.GetComponentInParent<PlayerActions>())
+        {
+
+            _animator.SetBool("activado", true);
+        }
+    }
+}
